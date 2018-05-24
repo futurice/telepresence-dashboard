80a35f937bfd03c468112b72391b77d6fb6f1966
Niki Ulmanen <niki.ulmanen@gmail.com>
2018-05-24 09:34:11 +0300

    Add gesture codes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
-
+var gestures = require('../data/gestures.json');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', gestures: gestures });
 });
 
 module.exports = router;
